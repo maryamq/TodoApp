457d9039e19f453f936fc67a750364f9e54744b8
Maryam Quadir <maryamquadir@gmail.com>
2014-08-04 09:49:50 -0700

    Basic todo

--- gen/com/maryamq/codepath/simpletodo/R.java
+++ gen/com/maryamq/codepath/simpletodo/R.java
@@ -10,24 +10,47 @@ package com.maryamq.codepath.simpletodo;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f060000;
-        public static final int Space1=0x7f060001;
-        public static final int btnAdd=0x7f060004;
-        public static final int etNewItem=0x7f060002;
-        public static final int lvItems=0x7f060003;
+        public static final int RelativeLayout1=0x7f080003;
+        public static final int Space1=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int btnAdd=0x7f080007;
+        public static final int button1=0x7f080002;
+        public static final int editText1=0x7f080000;
+        public static final int etNewItem=0x7f080005;
+        public static final int lvItems=0x7f080006;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_edit_item=0x7f030000;
+        public static final int activity_main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int edit_item=0x7f070000;
     }
     public static final class string {
-        public static final int add=0x7f040003;
-        public static final int add_new_item=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int action_settings=0x7f050005;
+        public static final int add=0x7f050003;
+        public static final int add_new_item=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int edit_item_below=0x7f050006;
+        public static final int hello_world=0x7f050001;
+        public static final int save=0x7f050007;
+        public static final int title_activity_edit_item=0x7f050004;
     }
     public static final class style {
         /** 
@@ -51,10 +74,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
